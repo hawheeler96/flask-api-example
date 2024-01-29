0019f7995fca9901c707918e39575cf7093a45ba
Holly Wheeler <hollyawheeler.96@gmail.com>
2024-01-29 13:18:32 -0800

    fixes models and seeding so db seeds properly

--- server/app.py
+++ server/app.py
@@ -1,2 +1,22 @@
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+
+from models import db, Attendee, Event, RSVP
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+
+@app.route("/")
+def index():
+    return "<h1>Events GET API</h1>"
+
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
